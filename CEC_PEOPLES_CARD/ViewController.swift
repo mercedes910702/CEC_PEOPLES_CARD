910d148aee15c6846a6074793f1099352a84e319
mercedes <mercedes.moore720@gmail.com>
2022-07-20 21:20:52 -0400

    Initial Commit

---
+++ CEC_PEOPLES_CARD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CEC_PEOPLES_CARD
+//
+//  Created by Mercedes Moore on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CEC_PEOPLES_CARDUITests/CEC_PEOPLES_CARDUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CEC_PEOPLES_CARDUITestsLaunchTests.swift
+//  CEC_PEOPLES_CARDUITests
+//
+//  Created by Mercedes Moore on 7/20/22.
+//
+
+import XCTest
+
+class CEC_PEOPLES_CARDUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
